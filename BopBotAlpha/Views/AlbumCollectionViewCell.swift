36026c1d25af8351f41a157d6d2bc65fb6eb653d
Piper Ward <piperward@pipers-macbook-pro.local>
2018-07-31 10:04:47 -0500

    Respond to album collection view cell selection

--- BopBotAlpha/Views/AlbumCollectionViewCell.swift
+++ BopBotAlpha/Views/AlbumCollectionViewCell.swift
@@ -13,4 +13,20 @@ class AlbumCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var collectionNameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     
+    override var isSelected: Bool{
+        didSet{
+            if self.isSelected
+            {
+                self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                self.collectionNameLabel.textColor = UIColor.gray
+                self.artistNameLabel.textColor = UIColor.gray
+            }
+            else
+            {
+                self.transform = CGAffineTransform.identity
+                self.collectionNameLabel.textColor = UIColor.white
+                self.artistNameLabel.textColor = UIColor.white
+            }
+        }
+    }
 }
